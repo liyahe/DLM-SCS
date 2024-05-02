2ee184c04f8ad5b1a31d510f4d274763b7a3fea9
liyahe <1014909671@qq.com>
2024-05-02 16:29:16 +0800

    add run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+bash scripts/batch_run_dlmscs.sh
+bash scripts/batch_run_dlmscs_maskOnly.sh
+bash scripts/batch_run_dlmscs_woTokenWeight.sh
+bash scripts/batch_run_finetune.sh
+bash scripts/batch_run_lmbff.sh
